f082d1a2bf1e8bf15579786e0b7bf977bfab7edd
Calado <wishsolidor@gmail.com>
2022-05-29 20:28:23 -0300

    feat: hide status bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:arcanth_shop/const.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'screens/home/home_screen.dart';
 
@@ -8,14 +10,17 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+    return MaterialApp(      
       debugShowCheckedModeBanner: false,
       title: 'Arcanth Shop',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         fontFamily: "Gordita",
+        scaffoldBackgroundColor: bgColor,
         textTheme: const TextTheme(
           bodyText2: TextStyle(color: Colors.black54),
         ),
