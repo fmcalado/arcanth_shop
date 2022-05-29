f76e0d4325f07d7039f75b5a1b9c2f222a726943
Calado <wishsolidor@gmail.com>
2022-05-29 20:28:58 -0300

    partial-feat: start homepage

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:arcanth_shop/components/home/body.dart';
+import 'package:arcanth_shop/components/home/header.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: headerComponent(context),
+      body: const HomeBody(),
+    );
   }
 }
