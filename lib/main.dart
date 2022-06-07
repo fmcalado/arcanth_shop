3da671df449462438dbd7d6b38b1b54234e7c794
Calado <wishsolidor@gmail.com>
2022-06-07 06:33:44 -0300

    feat: change routing system

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,48 @@
 import 'package:arcanth_shop/const.dart';
+import 'package:arcanth_shop/login_page.dart';
+import 'package:arcanth_shop/screens/cart/cart_screen.dart';
+import 'package:arcanth_shop/screens/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
+import 'package:get/get.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'screens/home/home_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: supabaseAPIURL,
+    anonKey: supabaseAnonKey,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
-    return MaterialApp(      
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Arcanth Shop',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: arcanthColor,
         fontFamily: "Gordita",
         scaffoldBackgroundColor: bgColor,
         textTheme: const TextTheme(
           bodyText2: TextStyle(color: Colors.black54),
         ),
       ),
-      home: const HomeScreen(),
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (_) => const SplashPage(),
+        '/login': (_) => const LoginPage(),
+        '/home': (_) => const HomeScreen(),
+        '/cart': (_) => CartScreen(),
+      },
+      // home: const HomeScreen(),
     );
   }
 }
