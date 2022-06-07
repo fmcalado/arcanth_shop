15b9414197c470060bf4c4fa9fc17e9a067dea66
Calado <wishsolidor@gmail.com>
2022-06-07 06:40:21 -0300

    feat: add product component

---
+++ lib/components/details/add_product.dart
@@ -0,0 +1,78 @@
+import 'package:arcanth_shop/const.dart';
+import 'package:arcanth_shop/controllers/cart_controller.dart';
+import 'package:arcanth_shop/models/product.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AddProduct extends StatefulWidget {
+  const AddProduct({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  final Product product;
+
+  @override
+  State<AddProduct> createState() => _AddProductState();
+}
+
+class _AddProductState extends State<AddProduct> {
+  final cartController = Get.put(CartController());
+  int _productCounter = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  if(_productCounter >= 2 ) _productCounter--;
+                });
+              },
+              child: const Text("-"),
+              style: ElevatedButton.styleFrom(
+                shape: const StadiumBorder(),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: defaultPadding, right: defaultPadding),
+              child: Text(
+                "$_productCounter",
+                style: Theme.of(context).textTheme.headline6,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _productCounter++;
+                });
+              },
+              child: const Text("+"),
+              style: ElevatedButton.styleFrom(
+                shape: const StadiumBorder(),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: defaultPadding),
+        SizedBox(
+          width: 200,
+          height: 48,
+          child: ElevatedButton(
+            onPressed: () {
+              for (var i = 0; i < _productCounter; i++) {
+                cartController.addProduct(widget.product);
+              }
+            },
+            child: const Text("Adicionar ao carrinho"),
+          ),
+        ),
+      ],
+    );
+  }
+}
