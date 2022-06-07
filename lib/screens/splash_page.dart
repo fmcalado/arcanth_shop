96fb64f2c0c7e45e8a1fa7275ed75d64208f5006
Calado <wishsolidor@gmail.com>
2022-06-07 06:36:30 -0300

    feat: finish auth

---
+++ lib/screens/splash_page.dart
@@ -0,0 +1,26 @@
+import 'package:arcanth_shop/components/auth_state.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends AuthState<SplashPage> {
+  @override
+  void initState() {
+    recoverSupabaseSession();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
